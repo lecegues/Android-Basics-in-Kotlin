d2cffbfcd686647af2a740c8b67ec0e5eb4b51d5
John Lecegues <68977770+lecegues@users.noreply.github.com>
2022-10-31 23:49:25 -0600

    Update 4.1 - Classes and Object Instances in Kotlin.kt

--- Unit 1: Kotlin Basics/4.1 - Classes and Object Instances in Kotlin.kt
+++ Unit 1: Kotlin Basics/4.1 - Classes and Object Instances in Kotlin.kt
@@ -1,5 +1,5 @@
-// Create a class with a passed parameter
-class Dice(var numSides: Int) {
+// Create a Dice class with a passed parameter
+class Dice(var numSides: Int, var color: String) {
     
  
     // function to roll the dice depending on how much sides there are
@@ -16,13 +16,48 @@ class Dice(var numSides: Int) {
     
 }
 
+// create a Coin class with the ability to flip
+class Coin(){
+    
+    fun flip(): String{
+        
+        val numFlipped = (0..1).random();
+        
+        // if 0, then heads. If 1, then tails. Anything else and we know an error occurred.
+        if (numFlipped == 0){
+            return "Heads";
+        }
+        else if (numFlipped == 1){
+            return "Tails";
+        }
+        else{
+            return "An error has occurred in Coin.flip() function";
+        }
+        
+    }
+        	
+        
+    
+}
+
 fun main(){
   
     // argument must be passed when creating object instance
-    val myFirstDice = Dice(5);
-    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}");
-    
-    myFirstDice.numSides = 20;
-    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}");
   
+    val originalDice = Dice(5, "red");
+    println("Your ${originalDice.color} ${originalDice.numSides} sided dice rolled ${originalDice.roll()}");
+    
+    // change value and reprint
+    originalDice.numSides = 10;
+    originalDice.color = "green";
+    println("Your ${originalDice.color} ${originalDice.numSides} sided dice rolled ${originalDice.roll()}");
+  	
+    println("-----------------------");
+    
+    val myCoin = Coin();
+  
+    println("You flipped a coin. It was ${myCoin.flip()}");
+    println("You flipped a coin. It was ${myCoin.flip()}");
+
+    
 }
